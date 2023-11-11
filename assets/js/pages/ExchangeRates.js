fedb2be1dd99fc450c3f4419c147443289ab4705
Daniel J <danielj@rockon.tech>
2023-11-12 00:23:10 +0100

    Create exchange rates page

---
+++ assets/js/pages/ExchangeRates.js
@@ -0,0 +1,62 @@
+import React, {useEffect, useState} from 'react'
+import Table from "../components/Table/Table"
+import Search from "../components/Search";
+import useAxiosWithAbort from "../hooks/useAxiosWithAbort";
+import {useHistory, useParams} from "react-router-dom";
+
+const baseUrl = 'http://telemedi-zadanie.localhost'
+
+const ExchangeRates = () => {
+    const history = useHistory()
+    const {date} = useParams()
+    const [prevDate, setPrevDate] = useState(date)
+    const [{currencies: todayCurrencies}] = useAxiosWithAbort(baseUrl + `/api/exchange-rates`)
+    const [
+        {currencies: historicalCurrencies},
+        historicalLoading,
+        historicalError,
+        historicalRefetch
+    ] = useAxiosWithAbort(baseUrl + `/api/exchange-rates/` + date)
+
+
+    const handleDateChange = (date) => {
+        history.push('/exchange-rates/' + date)
+    }
+
+    useEffect(() => {
+        if(prevDate === date) {
+            return
+        }
+
+        historicalRefetch()
+        setPrevDate(date)
+    }, [date])
+
+    return (<section className="row-section">
+        <div className="container-fluid px-4 pb-5">
+            <div className="row no-gutters mt-5">
+                <div className="col-md-12">
+                    <h1 className="text-center">Exchange Rates</h1>
+                </div>
+
+                <Search
+                    label="Date"
+                    value={date}
+                    onChange={handleDateChange}
+                />
+
+                <Table
+                    historicalCurrencies={historicalCurrencies || []}
+                    todayCurrencies={todayCurrencies || []}
+                    isLoading={historicalLoading}
+                    date={date}
+                />
+
+                {historicalError &&
+                    <div className="col-12 alert alert-danger mt-3 px-3" role="alert">{historicalError}</div>}
+            </div>
+        </div>
+    </section>)
+}
+
+export default ExchangeRates
--- assets/js/pages/Home.js
+++ assets/js/pages/Home.js
@@ -1,11 +1,15 @@
 // ./assets/js/components/Home.js
 
 import React, {Component} from 'react';
-import {Route, Redirect, Switch, Link} from 'react-router-dom';
+import {Route, Switch, Link} from 'react-router-dom';
 import SetupCheck from "./SetupCheck";
+import ExchangeRates from "./ExchangeRates";
+import {format} from "../helpers/date";
+
+const today = new Date()
+const options = [{year: 'numeric'}, {month: '2-digit'}, {day: '2-digit'}]
 
 class Home extends Component {
-
     render() {
         return (
             <div>
@@ -13,7 +17,10 @@ class Home extends Component {
                     <Link className={"navbar-brand"} to={"#"}> Telemedi Zadanko </Link>
                     <div id="navbarText">
                         <ul className="navbar-nav mr-auto">
-                            <li className="nav-item">
+                            <li className="nav-item d-flex">
+                                <Link className={"nav-link"} to={`/exchange-rates/${format(today, options)}`}>
+                                    Exchange Rates
+                                </Link>
                                 <Link className={"nav-link"} to={"/setup-check"}> React Setup Check </Link>
                             </li>
 
@@ -21,8 +28,8 @@ class Home extends Component {
                     </div>
                 </nav>
                 <Switch>
-                    <Redirect exact from="/" to="/setup-check" />
-                    <Route path="/setup-check" component={SetupCheck} />
+                    <Route path="/exchange-rates/:date" component={ExchangeRates}/>
+                    <Route path="/setup-check" component={SetupCheck}/>
                 </Switch>
             </div>
         )
