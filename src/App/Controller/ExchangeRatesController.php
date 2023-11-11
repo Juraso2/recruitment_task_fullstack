d09c8ed74caaded4062c939d0e7370be900d9d61
Daniel J <danielj@rockon.tech>
2023-11-12 00:18:06 +0100

    Add Api endpoints

--- src/App/Controller/ExchangeRatesController.php
+++ src/App/Controller/ExchangeRatesController.php
@@ -4,10 +4,63 @@ declare(strict_types=1);
 
 namespace App\Controller;
 
+use App\Service\CurrencyService;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 class ExchangeRatesController extends AbstractController
 {
+    private $currencyService;
 
+    public function __construct(CurrencyService $currencyService)
+    {
+        $this->currencyService = $currencyService;
+    }
 
+    public function exchangeRateIndex(): Response
+    {
+        try {
+            $currencies = $this->currencyService->fetchCurrencies();
+
+            $responseContent = [
+                'currencies' => $currencies
+            ];
+
+            return new JsonResponse($responseContent, Response::HTTP_OK);
+        } catch (\Exception $e) {
+            return new JsonResponse(['message' => $e->getMessage()], Response::HTTP_NOT_FOUND);
+        }
+    }
+
+    public function exchangeRateShow(string $code): Response
+    {
+        try {
+            $code = strtoupper($code);
+            $currency = $this->currencyService->fetchCurrency($code);
+
+            $responseContent = [
+                'currency' => $currency
+            ];
+
+            return new JsonResponse($responseContent, Response::HTTP_OK);
+        } catch (\Exception $e) {
+            return new JsonResponse(['message' => $e->getMessage()], Response::HTTP_NOT_FOUND);
+        }
+    }
+
+    public function exchangeRateIndexWithDate(string $date): Response
+    {
+        try {
+            $currencies = $this->currencyService->fetchCurrenciesWithDate($date);
+
+            $responseContent = [
+                'currencies' => $currencies
+            ];
+
+            return new JsonResponse($responseContent, Response::HTTP_OK);
+        } catch (\Exception $e) {
+            return new JsonResponse(['message' => $e->getMessage()], Response::HTTP_NOT_FOUND);
+        }
+    }
 }
