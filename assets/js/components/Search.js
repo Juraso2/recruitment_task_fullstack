13cb66f88bca114b42ab75ce091e9ac499828d01
Daniel J <danielj@rockon.tech>
2023-11-13 15:11:48 +0100

    Refactor Search to use new DatePicker component

--- assets/js/components/Search.js
+++ assets/js/components/Search.js
@@ -1,35 +1,16 @@
-import React, {useState} from "react"
+import React from "react"
 import PropTypes from "prop-types";
-import useAlert from "../hooks/useAlert";
+import DatePicker from "./DatePicker";
 
 const Search = ({label, value, onChange}) => {
-    const { showAlert } = useAlert()
-    const [prevDate, setPrevDate] = useState(value)
-
-    const handleDateChange = (event) => {
-        if (event.target.value === prevDate) {
-            return
-        }
-
-        const day = new Date(event.target.value).getUTCDay()
-        setPrevDate(event.target.value)
-
-        if (day === 0 || day === 6) {
-            showAlert('Weekends are not allowed', 'danger')
-            return
-        }
-
-        onChange(event.target.value)
+    const handleChange = (date) => {
+        if (date) onChange(date)
     }
 
     return (
         <div className="col-md-12 row no-gutters mt-5 mb-3 p-3 bg-light">
-            <div className="col-md-3 my-0 mb-2 mb-md-0 mr-md-2">
-                <div className="form-group mb-0">
-                    <label className="sr-only" htmlFor="date">{label}</label>
-                    <input type="date" className="form-control" id="date" min="2023-01-01" pattern="\d{4}-\d{2}-\d{2}"
-                           required value={value} onChange={handleDateChange}/>
-                </div>
+            <div className="col-lg-6 col-xl-4 form-group d-flex mb-0">
+                <DatePicker date={value} onChange={handleChange} className="flex-1" label={label}/>
             </div>
         </div>
     )
